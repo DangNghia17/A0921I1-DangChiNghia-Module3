a89b80f2056384ad67e18dca6cb951c2b9572419
Chi Nghia <dnghia2002@gmail.com>
2022-04-22 20:07:41 +0700

    20g07 22/04

---
+++ ss1_co_so_du_lieu_quan_he/quan_li_sinh_vien.sql
@@ -0,0 +1,24 @@
+create database ss1_relational_database;
+use ss1_relational_database;
+create table class(
+id int,
+`name` varchar(50)
+);
+create table teacher(
+id int,
+`name` varchar(50),
+age int ,
+country varchar(50)
+);
+
+
+
+-- create table student(
+-- id int,
+-- `name` varchar(50),
+-- age int ,
+-- country varchar(50)
+-- );
+
+
+
